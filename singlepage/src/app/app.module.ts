8680f185eb1546b5b0c15f90d87f3f3a046d9b11
unknown <pavankumar.samavedam@gmail.com>
2018-05-15 04:00:51 -0700

    test

---
+++ singlepage/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { MenubarComponent } from './menubar/menubar.component';
+import { LeftbodyComponent } from './leftbody/leftbody.component';
+import { RightbodyComponent } from './rightbody/rightbody.component';
+import { BannerComponent } from './banner/banner.component';
+import { EnquiryformComponent } from './rightbody/enquiryform/enquiryform.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenubarComponent,
+    LeftbodyComponent,
+    RightbodyComponent,
+    BannerComponent,
+    EnquiryformComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
